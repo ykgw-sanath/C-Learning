2bf1f48c188efa65cf63d8e1e4bc661b1f0a259b
Sanath Mugeraya <Sanath.Mugeraya@yokogawa.com>
2024-06-11 08:35:00 +0530

    pointers examples

---
+++ Day9-Functions/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sumCalculation(int a ,int b)
+{
+    int sum=a+b;
+
+    return sum;
+
+}
+int main()
+{
+int sum=sumCalculation(2,3);
+
+printf("%d",sum);
+
+return 0;
+}
